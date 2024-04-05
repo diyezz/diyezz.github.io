dd7eb6016ad827a68ec9f528a5d46b26c89dafbc
Dmytro Prokudin <pro.diyezz@yahoo.com>
2024-04-05 23:48:43 +0200

    commit2

--- main.js
+++ main.js
@@ -1,50 +1,61 @@
-"use strict";
+const pirateMap = [
+  ["f", "o", "o", "x", "o", "o"],
+  ["x", "o", "o", "o", "o", "o"],
+  ["o", "o", "x", "o", "o", "o"],
+  ["o", "x", "o", "o", "o", "x"],
+  ["o", "o", "o", "x", "x", "o"],
+  ["o", "x", "o", "o", "o", "s"],
+];
+
+const startGame = (arr) => {
+  const lastItem = arr.length - 1;
+  arr[lastItem][lastItem] = 'v';
+  return [lastItem, lastItem];
+};
+const renderChange = (id) => {
+  const gridContainer = document.getElementById(id);
+
+  pirateMap.forEach((row) => {
+    row.forEach((cellValue) => {
+      const cell = document.createElement("div");
+      cell.className = "cell " + cellValue;
+      cell.textContent = cellValue;
+      gridContainer.appendChild(cell);
+    });
+  });
+};
+renderChange("map-example");
 
 // Function declaration
 function moveForward(arr, x, y) {
-  arr[y - 1][x] = 'v';
+  arr[y - 1][x] = "v";
   return [x, y - 1];
 }
 
 // Fuction expression
-const turnLeft = function(arr, x, y) {
-  arr[y][x - 1] = 'v';
+const turnLeft = function (arr, x, y) {
+  arr[y][x - 1] = "v";
   return [x - 1, y];
-}
+};
 
 // Arrow function
 const turnRight = (arr, x, y) => {
-  arr[y][x + 1]= 'v';
+  arr[y][x + 1] = "v";
   return [x + 1, y];
-}
+};
 
-function generateMap(length) {
-  const subArray = new Array(length).fill('o')
-  const map = new Array(length).fill(subArray);
-  const lastIndex = length - 1;
-
-  const drawMap = map.map(el => {
-    const newEl = [...el];
-    const random = Math.floor(Math.random() * lastIndex);
-
-    newEl[random] = 'x';
-
-    return newEl;
-  });
-
-  drawMap[lastIndex][lastIndex] = 'v';
-
-  return drawMap;
-}
-
-
-const firstStep = turnLeft(pirateMap, 4, 4)
-const secondStep = moveForward(pirateMap, ...firstStep);
-const thirdStep = moveForward(pirateMap, ...secondStep);
-const fourthStep = moveForward(pirateMap, ...thirdStep);
+const gameStart = startGame(pirateMap);
+const step1 = turnLeft(pirateMap, ...gameStart);
+const step2 = turnLeft(pirateMap, ...step1);
+const step3 = turnLeft(pirateMap, ...step2);
+const step4 = moveForward(pirateMap, ...step3);
+const step5 = moveForward(pirateMap, ...step4);
+const step6 = turnRight(pirateMap, ...step5);
+const step7 = moveForward(pirateMap, ...step6);
+const fourthStep = moveForward(pirateMap, ...step7);
 const fifthStep = turnLeft(pirateMap, ...fourthStep);
 const sixtStep = turnLeft(pirateMap, ...fifthStep);
 const seventhStep = moveForward(pirateMap, ...sixtStep);
 const eightStep = turnLeft(pirateMap, ...seventhStep);
 
-console.log(pirateMap);
+renderChange("grid");
